6d3776392187bf0269d0076c473d7b1a19f91983
Ritwik <ritwikgoel@yahoo.com>
2021-06-26 13:15:09 +0530

    Feat: SceneBuilder

--- src/src/sample/Controller.java
+++ src/src/sample/Controller.java
@@ -1,13 +1,17 @@
 package sample;
 
+import com.spire.pdf.PdfDocument;
+import com.spire.pdf.PdfPageBase;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.stage.FileChooser;
-import javafx.stage.Window;
 
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class Controller {
 
@@ -27,12 +31,14 @@ public class Controller {
 
     }
     FileChooser fil_chooser = new FileChooser();
-    File file;
-    @FXML
-    void filesOpen(ActionEvent event) {
 
-        file = fil_chooser.showOpenDialog(sample.Main.primaryStage);
-        System.out.println(file);
+
+    @FXML
+    void filesOpen(ActionEvent event) throws IOException {
+
+        File filee = fil_chooser.showOpenDialog(sample.Main.primaryStage);
+        System.out.println(filee);
+
     }
 
 }
